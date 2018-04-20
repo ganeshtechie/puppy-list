abf667f134d14c645594d2146a29f7c491f95d07
Ganesh NR <nr.ganesh281990@gmail.com>
2018-04-20 11:12:57 +0530

    using multiple template as content

--- src/app/puppies/puppies.component.ts
+++ src/app/puppies/puppies.component.ts
@@ -5,16 +5,24 @@ import { Puppies } from '../data';
   selector: 'puppies-cmp',
   template: `
     <puppy-list [puppies]="puppies">
-      <ng-template let-puppy>
+
+      <ng-template let-pup="$implicit">
+
         <md-list-item>
-          <img mdListAvatar [src]="puppy.photo" alt="...">
-          <h3 mdLine> {{puppy.name}} </h3>
+          <img mdListAvatar [src]="pup.photo" alt="...">
+          <h3 mdLine> {{pup.name}} </h3>
           <p mdLine>
-            <span>Age: {{puppy.age}} </span>
-            <span>Breed: {{puppy.breed}} </span>
+            <span>Age: {{pup.age}} </span>
+            <span>Breed: {{pup.breed}} </span>
           </p>
         </md-list-item>
+
       </ng-template>
+
+      <ng-template let-size  #head>
+       <h1>Helo {{ size }}</h1>
+      </ng-template>
+
     </puppy-list>
   `
 })
--- src/app/puppy-list/puppy-list.component.ts
+++ src/app/puppy-list/puppy-list.component.ts
@@ -1,17 +1,48 @@
 import { Puppy } from './../puppy/puppy';
-import { Component, Input, TemplateRef, ContentChild } from '@angular/core';
+import { Component, Input, TemplateRef, ContentChild, ViewChild, AfterViewInit, ViewContainerRef } from '@angular/core';
 import { NgForOfContext } from '@angular/common';
 
 @Component({
   selector: 'puppy-list',
   template: `
     <md-list>
+
+      <ng-container #head>
+      </ng-container>
+
       <ng-template ngFor let-puppy [ngForOf]="puppies" [ngForTemplate]="puppyTemplate">
       </ng-template>
     </md-list>
   `
 })
-export class PuppyListComponent {
-  @Input() puppies: Puppy[];
+export class PuppyListComponent  {
+
+
+  @Input() puppies: Puppy[];   
+  
   @ContentChild(TemplateRef) puppyTemplate: TemplateRef<NgForOfContext<Puppy>>;
+
+  @ViewChild("head", { read: ViewContainerRef }) head: ViewContainerRef;
+
+  @ContentChild("head", { read: TemplateRef }) headTemplate: TemplateRef<PuppyListContext>;
+
+
+  ngAfterViewInit(){
+
+    Promise.resolve().then(() => {
+      
+      this.head.createEmbeddedView(this.headTemplate, { $implicit : 10 });
+      
+    });
+
+  }
+  
+  
+  
+
+}
+
+
+export class PuppyListContext {
+  $implicit: number;
 }
